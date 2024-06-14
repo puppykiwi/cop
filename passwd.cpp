ecae7d4708ab577fc742492854d903ff2c238bfe
puppykiwi <johnraymwendwa04@gmail.com>
2024-06-14 11:00:00 +0300

    new

---
+++ passwd.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(){
+     
+    int length = get_length()
+}
+
+int get_length(){
+
+    
+    std::cout << "What is the desired length of your program: ";
+    std::cin >> int size;
+    
+}
