d81412b3b35931b1853888187ed61246a5733d0a
puppykiwi <johnraymwendwa04@gmail.com>
2024-06-14 11:08:39 +0300

    .

--- passwd.cpp
+++ passwd.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
+#include "passwd.h"
 
 int main(){
      
-    int length = get_length()
+    int length = get_length();
 }
 
 int get_length(){
 
-    
+    int size;
     std::cout << "What is the desired length of your program: ";
-    std::cin >> int size;
+    std::cin >> size;
     
+    if typeid(size).name
 }
